76f17224b495c3502517d086fdec072af1b33255
Ayush <ayush.vishwakarma2016@gmail.com>
2024-06-25 16:57:33 +0530

    day_17_2

---
+++ day_017_prob_543.py
@@ -0,0 +1,11 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = [0]
+
+        def dfs(root):
+            if not root:
+                return -1
+            l = dfs(root.left)
+            r = dfs(root.right)
+            res[0] = max(res[0], 2+l+r)
+            return 1+max(l,r)
