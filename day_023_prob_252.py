4240676689235a2031480e60488451cbb6bfeaf8
Ayush <ayush.vishwakarma2016@gmail.com>
2024-07-03 18:22:31 +0530

    day_23_4

---
+++ day_023_prob_252.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda i: i.start)
+
+        for i in range(1, len(intervals)):
+            i1 = intervals[i - 1]
+            i2 = intervals[i]
+
+            if i1.end > i2.start:
+                return False
+        return True
